41569fa58b8a48548352019ad4fae82228d901b4
Emily Garcia <egarcia83@mail.pima.edu>
2018-10-09 06:34:35 -0700

    Added base action and base factor script

---
+++ Assets/Scripts/BaseAction.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[Serializable]
+public class BaseAction : MonoBehaviour
+{
+    public int BaseWeight;
+
+    public float SecondsToCacheRank = 10f;
+
+    private int _weightBonus;
+
+    private int _rank;
+
+    private float _nextChacheTime;
+
+    //Factors to consider for this action
+    public List<BaseFactor> Factors;
+
+    //Action that will be performed if this is picked
+    public AIAction ActionToPerform;
+
+    virtual public int GetRank(GameObject aiToCheck)
+    {
+        if (_rank > 0 && _nextChacheTime > Time.timeSinceLevelLoad)
+        {
+            return _rank;
+        }
+        _nextChacheTime = Time.timeSinceLevelLoad + SecondsToCacheRank;
+
+        _rank = 0;
+
+        foreach (var factor in Factors)
+        {
+            if (factor.GetFactorRank(aiToCheck) > _rank)
+            {
+                _rank += factor.GetFactorRank(aiToCheck);
+            }
+        }
+
+        return _rank;
+    }
+
+    virtual public int GetWeight(GameObject aiToCheck)
+    {
+        //sum of bonuses * product of multipliers
+        _weightBonus = 0;
+
+        foreach (var factor in Factors)
+        {
+            _weightBonus += factor.GetFactorBonus(aiToCheck) * factor.GetFactorMultiplier(aiToCheck);
+        }
+        return _weightBonus;
+    }
+
+    virtual public void Run(GameObject ai) { }
+}
---
+++ Assets/Scripts/BaseFactor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[Serializable]
+public class BaseFactor : MonoBehaviour
+{
+
+    virtual public int GetFactorRank(GameObject aiToCheck)
+    {
+        //default rank is 0
+        return 0;
+    }
+
+    virtual public int GetFactorBonus(GameObject aiToCheck)
+    {
+        //default bonus is 0
+        return 0;
+    }
+
+    virtual public int GetFactorMultiplier(GameObject aiToCheck)
+    {
+        //default multiplier is 0
+        return 1;
+    }
+}
