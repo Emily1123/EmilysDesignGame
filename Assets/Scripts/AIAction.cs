4ac5d83c474cb285180bd0f6fe3c2d73079c07ea
Emily Garcia <egarcia83@mail.pima.edu>
2018-10-09 07:33:10 -0700

    Changed AI action script to include enums

--- Assets/Scripts/AIAction.cs
+++ Assets/Scripts/AIAction.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class AIAction : MonoBehaviour
 {
+    public enum Factors { None, Distance, Threat, AttackDesire};
+    Factors currentDecision;
     public void ActionToPerform()
     {
-
+        currentDecision = Factors.None;
     }
 }
